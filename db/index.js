ef9d1012dc8939ca9bc42bf550ae441dfb34b51e
Tyler <tylercaillet2121@gmail.com>
2022-10-21 11:31:24 -0400

    updated index.js

--- db/index.js
+++ db/index.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose')
 
+let MONGODB_URI = 'mongodb://127.0.0.1:27017/covidDatabase'
+
 mongoose
-  .connect('mongodb://127.0.0.1:27017/covidDatabase')
+  .connect(MONGODB_URI)
   .then(() => {
     console.log('Successfully connected to MongoDB')
   })
   .catch((e) => {
     console.log('Connection error', e.message)
   })
-mongoose.set('debug', true)
+
 const db = mongoose.connection
 
 module.exports = db
