b39b3804fb6877d4fbec5b11c71bddf75f4e6b7c
Tyler <tylercaillet2121@gmail.com>
2022-10-21 15:16:02 -0400

    updates

--- models/covidDocuments.js
+++ models/covidDocuments.js
@@ -3,9 +3,9 @@ const { Schema } = require('mongoose')
 const CovidDocuments = new Schema(
   {
     title: { type: String, default: null },
-    DOB: { type: String, required: true },
-    first: { type: String, required: true },
-    second: { type: String, required: true },
+    DOB: { type: String, required: false },
+    first: { type: String, required: false },
+    second: { type: String, default: null },
     userOne_id: { type: Schema.Types.ObjectId, ref: 'userOne_id' }
   },
   { timestamps: true }
--- seed/covidDocument.js
+++ seed/covidDocument.js
@@ -4,33 +4,31 @@ const { UserOne, CovidDocuments } = require('../models')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
-  const tylersDocuments = await UserOne.find({ firstName: 'Tyler' })
-
   const documents = [
     {
       title: 'Vaccination Card',
       DOB: 'June 8th, 1995',
       first: 'pfizer; May 13th, 2021',
       second: 'pfizer; June 6th, 2021',
-      userOne_id: tylersDocuments[0]._id
+      userOne_id: '6352ed508787df7110d829b2'
     },
     {
       title: 'Booster Info',
       DOB: 'June 8th, 1995',
       info: 'pfizer; October 3rd, 2021',
-      userOne_id: tylersDocuments[0]._id
+      userOne_id: '6352ed508787df7110d829b2'
     },
     {
       title: 'Last Covid Test',
       Date: 'October 21st, 2022',
       info: 'iHealth Covid-19 Antigen Rapid Test',
-      userOne_id: tylersDocuments[0]._id
+      userOne_id: '6352ed508787df7110d829b2'
     },
     {
       title: 'Last tested positive for Covid',
       Date: 'July 6th, 2022',
       info: 'iHealth Covid-19 Antigen Rapid Test',
-      userOne_id: tylersDocuments[0]._id
+      userOne_id: '6352ed508787df7110d829b2'
     }
   ]
 
