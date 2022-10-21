ab1acdd6e727460fc4ca2b6e7c5c08d8332b33f4
Tyler <tylercaillet2121@gmail.com>
2022-10-21 11:28:15 -0400

    updated /routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 const { Router } = require('express')
+const controllers = require('../controllers')
 const router = Router()
 
 router.get('/', (req, res) => res.send('This is the router'))
+
+router.get('/covids', controllers.getAllCovid)
+
+router.get('/covids/:id', controllers.getCovidById)
+
+router.post('/covids', controllers.createCovid)
+
+router.put('/covids/:id', controllers.updateCovid)
+
+router.delete('/covids/:id', controllers.deleteCovid)
 
 module.exports = router
