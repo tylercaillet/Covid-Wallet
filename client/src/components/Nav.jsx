985d1bdd68f288d874817f194746a5d8c692660b
Tyler <tylercaillet2121@gmail.com>
2022-10-24 15:57:05 -0400

    updates

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -6,11 +6,16 @@ const Nav = () => {
    return ( <div>
          
     <div className='nav-container'>
-        <img src='https://www.uchealth.com/content/dam/uchealth/images/media-room/covid-19/new-covid-19-strain.jpg/jcr:content/renditions/cq5dam.web.767.431.jpg' alt='website logo' className='logo'/>
+    <header>
         <div>
             <Link to = '/' className='link'> Login </Link>
             <Link to = '/userOne' className='link'> Sign up </Link>
+            <h1 className="Covid">Welcome to your Covid Wallet</h1>
         </div>
+        </header>
+        <img src='https://www.uchealth.com/content/dam/uchealth/images/media-room/covid-19/new-covid-19-strain.jpg/jcr:content/renditions/cq5dam.web.767.431.jpg' alt='website logo' className='logo'/>
+        <img src='https://cdn.shopify.com/s/files/1/0623/8034/3544/products/AirTagSlideWallet-CarbonFiber_720x.jpg?v=1642725309' alt='website logo' className='wallet-logo'/>
+        
     </div>
    </div> )   
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,9 +2,20 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div className="homePage">
-      <h1> Welcome to your Covid Wallet</h1>
-    </div>
+    <header>
+      <div className="homePage">
+        <h1>
+          {' '}
+          Have you had a chance to travel or experience some sort of normality
+          since Covid hit? It can be frustating to have to provide proof that
+          you're "well enough" to do any of those things. Well with the
+          Covid-Wallet you can neatly and safely store any and all pertinent
+          information that you may need. You can store your vaccination card,
+          your booster information, when you last recived a Covid-19 test, and
+          lastly when was the last time you were diagnosed postive with Covid.
+        </h1>
+      </div>
+    </header>
   )
 }
 
