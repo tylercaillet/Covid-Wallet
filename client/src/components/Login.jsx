d045ed033c11fea07dc4757d9341be4863c89878
Tyler <tylercaillet2121@gmail.com>
2022-10-26 09:40:05 -0400

    Login updates

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,45 @@
-const Login = () => {
+import React, { useState } from 'react';
+
+const LoginForm = () => {
+    const initialState = { username: '' };
+    const [formState, setFormState] = useState(initialState);
+
+    const handleChange = event => {
+       setFormState({ ...formState, [event.target.id]: event.target.value });
+    };
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(formState);
+        setFormState(initialState);
+    }
     return (
-        <div className='userlogin'>Hello</div>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input
+            id="username"
+            type="text"
+            onChange={handleChange}
+            value={formState.username}
+            />
+            <label htmlFor="password">Password:</label>
+            <input
+            id="password"
+            type="password"
+            onChange={handleChange}
+            value={formState.password}
+            />
+            <button type="submit">Login</button>
+        </form>
     )
 }
+
+
+// const Login = () => {
+//     return (
+//         <div className='userlogin'>Hello</div>
+//     )
+// }
 
 
 export default Login
