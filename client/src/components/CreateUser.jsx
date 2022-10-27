07cde7db72bbc35707e45866ac7f9d7b2414afff
Tyler <tylercaillet2121@gmail.com>
2022-10-27 09:58:28 -0400

    updates

--- client/src/components/CreateUser.jsx
+++ client/src/components/CreateUser.jsx
@@ -1,7 +1,48 @@
+import React, { useState } from 'react';
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
 const CreateUser = () => {
+    let navigate = useNavigate()
+    const initialState = { username: '', password: '' };
+    const [formState, setFormState] = useState(initialState);
+
+    const handleChange = event => {
+       setFormState({ ...formState, [event.target.id]: event.target.value });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        let res = await axios.post('http://localhost:3001/api/userOne', formState)
+        console.log(res.data);
+        setFormState(initialState);
+        navigate(`/Login`)
+    }
     return (
-        <div className='create-user'>Sign Up</div>
+        <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="username" className='userName'>Username:</label>
+            <input
+            id="username"
+            type="text"
+            onChange={handleChange}
+            value={formState.username}
+            />
+            <label htmlFor="password" className='password'>Password:</label>
+            <input
+            id="password"
+            type="password"
+            onChange={handleChange}
+            value={formState.password}
+            />
+            <button type="submit">Create new user</button>
+        </form>
+        </div>
     )
 }
+
+
+
+
 
 export default CreateUser
