2ebe56077dce445e6b9c54fc6781e76a0dd6bf88
Tyler <tylercaillet2121@gmail.com>
2022-11-03 21:15:20 -0400

    please work

--- db/index.js
+++ db/index.js
@@ -1,18 +1,20 @@
 const mongoose = require('mongoose')
+require('dotenv').config() // Add this line
 
-let MONGODB_URI =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/covidDatabase'
+let dbUrl =
+  process.env.NODE_ENV === 'production'
+    ? process.env.MONGODB_URI
+    : 'mongodb://127.0.0.1:27017/<database_name>'
 
 mongoose
-  .connect(MONGODB_URI)
+  .connect(dbUrl)
   .then(() => {
-    console.log('Successfully connected to MongoDB')
+    console.log('Successfully connected to MongoDB!')
   })
   .catch((e) => {
-    console.log('Connection error', e.message)
+    console.error('Connection error', e.message)
   })
 mongoose.set('debug', true)
-
 const db = mongoose.connection
 
 module.exports = db
